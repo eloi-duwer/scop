866f3b2666cee9114c63f18427a1272efe6b852f
eduwer <eloitrocy@hotmail.fr>
2021-05-22 17:33:28 +0200

    headers OK

--- include/gl_init.h
+++ include/gl_init.h
@@ -6,53 +6,53 @@
 /*   By: eduwer <eduwer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 13:37:24 by eduwer            #+#    #+#             */
-/*   Updated: 2021/05/22 17:08:15 by eduwer           ###   ########.fr       */
+/*   Updated: 2021/05/22 17:29:06 by eduwer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __GL_INIT_H__
 # define __GL_INIT_H__
 
-#define GL_FALSE					0
-#define GL_TRUE						1
+# define GL_FALSE					0
+# define GL_TRUE					1
 
-#define GL_NO_ERROR					0
+# define GL_NO_ERROR				0
 
-#define GL_UNSIGNED_BYTE			0x1401
-#define GL_UNSIGNED_INT				0x1405
-#define GL_FLOAT					0x1406
+# define GL_UNSIGNED_BYTE			0x1401
+# define GL_UNSIGNED_INT			0x1405
+# define GL_FLOAT					0x1406
 
-#define GL_DEPTH_BUFFER_BIT			0x00000100
-#define GL_COLOR_BUFFER_BIT			0x00004000
+# define GL_DEPTH_BUFFER_BIT		0x00000100
+# define GL_COLOR_BUFFER_BIT		0x00004000
 
-#define GL_TEXTURE_2D				0x0DE1
+# define GL_TEXTURE_2D				0x0DE1
 
-#define GL_DEPTH_TEST				0x0B71
+# define GL_DEPTH_TEST				0x0B71
 
-#define GL_TRIANGLES				0x0004
+# define GL_TRIANGLES				0x0004
 
-#define GL_LESS						0x0201
+# define GL_LESS					0x0201
 
-#define GL_LINEAR					0x2601
+# define GL_LINEAR					0x2601
 
-#define GL_RGB						0x1907
-#define GL_BGR						0x80E0
+# define GL_RGB						0x1907
+# define GL_BGR						0x80E0
 
-#define GL_TEXTURE_MAG_FILTER		0x2800
-#define GL_TEXTURE_MIN_FILTER		0x2801
-#define GL_TEXTURE_WRAP_S			0x2802
-#define GL_TEXTURE_WRAP_T			0x2803
-#define GL_REPEAT					0x2901
+# define GL_TEXTURE_MAG_FILTER		0x2800
+# define GL_TEXTURE_MIN_FILTER		0x2801
+# define GL_TEXTURE_WRAP_S			0x2802
+# define GL_TEXTURE_WRAP_T			0x2803
+# define GL_REPEAT					0x2901
 
-#define GL_ARRAY_BUFFER				0x8892
-#define GL_ELEMENT_ARRAY_BUFFER		0x8893
-#define GL_STATIC_DRAW				0x88E4
+# define GL_ARRAY_BUFFER			0x8892
+# define GL_ELEMENT_ARRAY_BUFFER	0x8893
+# define GL_STATIC_DRAW				0x88E4
 
-#define GL_FRAGMENT_SHADER			0x8B30
-#define GL_VERTEX_SHADER			0x8B31
-#define GL_COMPILE_STATUS			0x8B81
-#define GL_LINK_STATUS				0x8B82
-#define GL_INFO_LOG_LENGTH			0x8B84
+# define GL_FRAGMENT_SHADER			0x8B30
+# define GL_VERTEX_SHADER			0x8B31
+# define GL_COMPILE_STATUS			0x8B81
+# define GL_LINK_STATUS				0x8B82
+# define GL_INFO_LOG_LENGTH			0x8B84
 
 typedef unsigned char	GLboolean;
 typedef char			GLchar;
@@ -75,7 +75,7 @@ typedef signed long int	GLsizeiptr;
 ** Used to avoid to repeat all the declarations
 */
 
-#define OPENGL_FUNCS \
+# define OPENGL_FUNCS \
 GLFUNC(void, EnableVertexAttribArray, GLuint index) \
 GLFUNC(void, BindBuffer, GLenum targer, GLuint buffer) \
 GLFUNC(void, VertexAttribPointer, GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void *pointer) \
@@ -115,15 +115,25 @@ GLFUNC(void, TexImage2D, GLenum target, GLint level, GLint internalformat, GLsiz
 GLFUNC(void, TexParameteri, GLenum target, GLenum pname, GLint param) \
 GLFUNC(void, GenerateMipmap, GLenum target) \
 
-#define GLFUNC(ret, name, ...) typedef ret name##Proc(__VA_ARGS__); extern name##Proc *gl##name;
-OPENGL_FUNCS
-#undef GLFUNC
+/*
+** Declare the functions as extern
+** So that we have only one definition of them
+*/
 
-#ifdef GLFUNC_IMPL
-#define GLFUNC(ret, name, ...) typedef ret name##Proc(__VA_ARGS__); name##Proc *gl##name;
+# define GLFUNC(ret, name, ...) typedef ret name##Proc(__VA_ARGS__); extern name##Proc *gl##name;
 OPENGL_FUNCS
-#undef GLFUNC
-#endif
+# undef GLFUNC
+
+/*
+** Declare all the functions,
+** The define GLFUNC_IMPL must be present in only one file (in load_opengl_funcs.c)
+*/
+
+# ifdef GLFUNC_IMPL
+#  define GLFUNC(ret, name, ...) typedef ret name##Proc(__VA_ARGS__); name##Proc *gl##name;
+OPENGL_FUNCS
+#  undef GLFUNC
+# endif
 
 int		load_opengl_funcs(void);
 
