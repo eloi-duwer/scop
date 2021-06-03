cf27ec64def98723b90be1f0fe44c042bf248e0f
eduwer <eloitrocy@hotmail.fr>
2021-06-03 20:25:11 +0200

    Fixed problems with object reloading

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: eduwer <eduwer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 00:59:54 by eduwer            #+#    #+#             */
-/*   Updated: 2021/06/03 17:08:10 by eduwer           ###   ########.fr       */
+/*   Updated: 2021/06/03 20:22:19 by eduwer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,13 @@ static void	render_obj(t_context *ctx, t_object *obj)
 	if (ctx->rotating)
 		rotation_matrix_y(&obj->rotation_matrix, 0.003);
 	glUseProgram(ctx->prog_object);
-
+	glBindVertexArray(obj->vertex_array);
+	glBindBuffer(GL_ARRAY_BUFFER, obj->vertex_buffers[0]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, obj->vertex_buffers[1]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, obj->texture);
 	glDepthFunc(GL_LESS);
-
 	glUniform3f(ctx->min_pos_handle, obj->min_coords.x, obj->min_coords.y, obj->min_coords.z);
 	glUniform3f(ctx->max_pos_handle, obj->max_coords.x, obj->max_coords.y, obj->max_coords.z);
 	glUniform1i(ctx->has_uv_coords, GL_FALSE);
@@ -51,30 +53,29 @@ static void	render_obj(t_context *ctx, t_object *obj)
 	mat4x4_mult(&obj->model_matrix, &ctx->cam.view_matrix, &obj->mvp_matrix);
 	mat4x4_mult(&obj->mvp_matrix, &ctx->cam.projection_matrix, &obj->mvp_matrix);
 	glUniformMatrix4fv(obj->mvp_handle, 1, GL_TRUE, &obj->mvp_matrix.mat[0][0]);
-	glBindVertexArray(obj->vertex_array);
 	print_opengl_error("before drawElements object");
 	glDrawElements(GL_TRIANGLES, obj->nb_faces * 3, GL_UNSIGNED_INT, (void *)0);
-	print_opengl_error("after drawElements object");
 	glBindVertexArray(0);
+	print_opengl_error("after drawElements object");
 }
 
 static void	render_skybox(t_context *ctx, t_object *skybox)
 {
 	print_opengl_error("Before rendering skybox");
 	glUseProgram(ctx->prog_skybox);
-
+	glBindVertexArray(skybox->vertex_array);
+	glBindBuffer(GL_ARRAY_BUFFER, skybox->vertex_buffers[0]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skybox->vertex_buffers[1]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, ctx->skybox.texture);
 	glDepthFunc(GL_LEQUAL);
-
 	construct_model_matrix(ctx, skybox, 1);
 	mat4x4_mult(&skybox->model_matrix, &ctx->cam.view_matrix, &skybox->mvp_matrix);
 	mat4x4_mult(&skybox->mvp_matrix, &ctx->cam.projection_matrix, &skybox->mvp_matrix);
 	glUniformMatrix4fv(skybox->mvp_handle, 1, GL_TRUE, &skybox->mvp_matrix.mat[0][0]);
-	glBindVertexArray(skybox->vertex_array);
-	print_opengl_error("before drawElements object");
+	print_opengl_error("before drawElements skybox");
 	glDrawElements(GL_TRIANGLES, skybox->nb_faces * 3, GL_UNSIGNED_INT, (void *)0);
-	print_opengl_error("after drawElements object");
 	glBindVertexArray(0);
 	print_opengl_error("After rendering skybox");
 }
